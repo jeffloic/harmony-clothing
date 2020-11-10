8157afe0c4e9f09e2d7380e4277c94d51676d340
Loic <loicnondah@gmail.com>
2020-11-10 12:37:34 +0000

    update our menu-items to navigate with React Router

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,7 +5,7 @@ import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 
 
-const HomePage = () => (
+const HomePage = ({history}) => (
     <div className="homepage">
         <Directory />
     </div>
